acb45a4b45eae57c5637192cb7fdec50eae3fa5e
Agus Sudarta <69590915+tazumii@users.noreply.github.com>
2022-11-26 16:42:06 +0100

    update file structure

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import RootStackNavigator from "./navigation/RootStackNavigator";
+import RootStackNavigator from "./src/navigation/RootStackNavigator";
 
 export default function App() {
   return (
